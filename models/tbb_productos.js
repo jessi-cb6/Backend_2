5d28cd22f4ad80b6f73113fe58d505e4e0e40a12
Weildor <240065@utxicotepec.edu.mx>
2026-04-12 15:23:36 -0600

    Conexion Corregida y sin errores con la base de datos y Relaciones de las tablas, codigo corregido

--- models/tbb_productos.js
+++ models/tbb_productos.js
@@ -1,4 +1,36 @@
 'use strict';
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class tbb_productos extends Model {
+    static associate(models) {
+      // Un producto PERTENECE A una categoría
+      tbb_productos.belongsTo(models.tbc_categorias, {
+        foreignKey: 'id_categoria', // F minúscula
+        as: 'categoria'
+      });
+
+      // Un producto puede estar en MUCHOS detalles de carrito
+      tbb_productos.hasMany(models.tbc_carrito_detalle, {
+        foreignKey: 'id_productos',
+        as: 'detalles_carrito'
+      });
+    }
+  }
+  tbb_productos.init({
+    nombre: DataTypes.STRING,
+    descripcion: DataTypes.STRING,
+    precio: DataTypes.STRING,
+    stock: DataTypes.STRING,
+    id_categoria: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'tbb_productos',
+  });
+
+  return tbb_productos;
+};
+/*'use strict';
 const {
   Model
 } = require('sequelize');
@@ -9,7 +41,7 @@ module.exports = (sequelize, DataTypes) => {
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
-     */
+     *
     static associate(models) {
       // define association here
     }
@@ -38,3 +70,4 @@ module.exports = (sequelize, DataTypes) => {
   }
   return tbb_productos;
 };
+*/
--- models/tbc_categorias.js
+++ models/tbc_categorias.js
@@ -1,4 +1,27 @@
 'use strict';
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class tbc_categorias extends Model {
+    static associate(models) {
+      // Una categoría TIENE MUCHOS productos
+      tbc_categorias.hasMany(models.tbb_productos, {
+        foreignKey: 'id_categoria', // F minúscula
+        as: 'productos'
+      });
+    }
+  }
+  tbc_categorias.init({
+    nombre: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'tbc_categorias',
+  });
+  
+  return tbc_categorias;
+};
+/*
+'use strict';
 const {
   Model
 } = require('sequelize');
@@ -8,7 +31,7 @@ module.exports = (sequelize, DataTypes) => {
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
-     */
+     *
     static associate(models) {
       // define association here
     }
@@ -27,3 +50,4 @@ module.exports = (sequelize, DataTypes) => {
   }
   return tbc_categorias;
 };
+*/
